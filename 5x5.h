65048ecfe98d08666927d7bd050c85bfd9b531ba
8dcc <8dcc.git@gmail.com>
2023-04-13 10:29:12 +0200

    Move defines and structs to source

--- 5x5.h
+++ 5x5.h
@@ -1,10 +1,6 @@
 #ifndef _APPS_5x5_H
 #define _APPS_5x5_H 1
 
-#include <stdint.h>
-
-/** @todo: Move defines and structs to source */
-
 /**
  * @brief Entry point of the 5x5 game
  * @param argc Number of arguments
@@ -12,73 +8,5 @@
  * @return Exit code
  */
 int main_5x5(int argc, char** argv);
-
-/**
- * @def CHEAT
- * @brief Compile with cheater features.
- * @details If the program is compiled with this macro defined, it will add the
- * "Generate cheat grid" feature, which generates a very easy game for testing.
- */
-#define CHEAT
-
-/**
- * @def USE_ARROWS
- * @brief Compile with arrow navigation.
- * @details If the program is compiled with this macro defined, it will support
- * arrow navigation. Currently unsupported.
- */
-/* #define USE_ARROWS */
-
-#define DEFAULT_H   5 /**< @brief Default height */
-#define DEFAULT_W   5 /**< @brief Default width */
-#define DEFAULT_YSC 3 /**< @brief Default vertical scale */
-#define DEFAULT_XSC 4 /**< @brief Default horizontal scale */
-#define MIN_W       5 /**< @brief Minimum width */
-#define MIN_H       5 /**< @brief Minimum height */
-
-/**
- * @def KEY_CTRLC
- * @brief Needed for getch()
- */
-#define KEY_CTRLC 3
-
-/**
- * @enum game_chars
- * @brief Characters for the tiles
- */
-enum game_chars {
-    ON_CH  = '#', /**< @brief On tile */
-    OFF_CH = '.', /**< @brief Off tile */
-};
-
-/**
- * @struct point_t
- * @brief Point in the **context grid**.
- * @details Not a point on the real terminal, but on the grid. This is useful
- * for moving to the real terminal position after rendering the scalled grid.
- */
-typedef struct {
-    uint16_t y; /**< @brief Y coordinate */
-    uint16_t x; /**< @brief X coordinate */
-} point_t;
-
-/**
- * @struct ctx_t
- * @brief Context used for the game.
- */
-typedef struct {
-    uint16_t h;     /**< @brief Grid height */
-    uint16_t w;     /**< @brief Grid width */
-    uint16_t ysc;   /**< @brief Vertical grid scale for rendering */
-    uint16_t xsc;   /**< @brief Horizontal scale for rendering */
-    point_t cursor; /**< @brief User position inside the grid */
-
-    /**
-     * @brief Pointer to the game grid.
-     * @details It will save 1 char per cell, scale will be used for rendering
-     * only.
-     * */
-    uint8_t* grid;
-} ctx_t;
 
 #endif /* _APPS_5x5_H */
